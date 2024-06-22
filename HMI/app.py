d792a7338de84d1e099e09816b29592c680c90ed
Dr-Daily <56745553+Dr-Daily@users.noreply.github.com>
2024-06-22 10:30:32 -0600

    Unified into a multi-threaded flask app

--- HMI/app.py
+++ HMI/app.py
@@ -1,20 +1,24 @@
 #!/python
 from flask import Flask, jsonify,render_template, request, send_file
+from flask_socketio import SocketIO, emit #sudo apt install python3-flask-socketio -y
 import csv
 import io
 import subprocess
-import humanize #sudo apt install python3-humanize
-from flask_cors import CORS
-import psutil
+import humanize #sudo apt install python3-humanize -y
+import psutil #sudo apt install python3-psutil -y
 import socket
 import sqlite3
 import re
 import time
+import threading
+import queue
+import logging
+import struct
 
 DATABASE = 'can_messages.db'
 
 app = Flask(__name__)
-CORS(app)
+socketio = SocketIO(app,async_mode='threading', cors_allowed_origins="*")
 
 # After making changes, run 
 # sudo systemctl restart flask-app.service
@@ -26,6 +30,180 @@ CORS(app)
 # If that's not enough, run
 # journalctl -r 
 # to get the tail end of the system journal
+
+# Define the queues
+raw_data_queue = queue.Queue()
+processed_data_queue = queue.Queue()
+summary_data_queue = queue.Queue(2) # this is a holding queue for data. 
+
+# Setup CAN interfaces (adjust the settings according to your hardware)
+#can_interfaces = ['can0', 'can1']
+can_interfaces = ['can0',]
+can_bitrates = [250000,]
+
+summary_data = {"total_count":0}
+for i in can_interfaces:
+    summary_data[i] = { "name":f"{i}",
+                        "count":0,
+                        "source":{}
+                       }
+                
+
+# Control flags for logging
+logging_active = threading.Event()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='app.py %(levelname)s:%(message)s')
+logger = logging.getLogger()
+
+UPDATE_PERIOD = 0.91  # seconds
+STATS_UPDATE_PERIOD = 0.29
+
+# Struct format for CAN frame
+can_frame_format = "<lB3x8s"
+
+# J1939 bit masks and shifts
+PRIORITY_MASK = 0x1C000000
+EDP_MASK = 0x02000000
+DP_MASK = 0x01000000
+PF_MASK = 0x00FF0000
+PS_MASK = 0x0000FF00
+SA_MASK = 0x000000FF
+PDU1_PGN_MASK = 0x03FF0000
+PDU2_PGN_MASK = 0x03FFFF00
+
+def read_can_data(interface):
+    # Create a raw socket and bind it to the CAN interface
+    sock = socket.socket(socket.AF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
+    sock.bind((interface,))
+
+    while True:
+        try:
+            can_packet = sock.recv(16)
+        except OSError as e:
+            logger.info(f"Reading {interface} failed with error {e}")
+            time.sleep(1)
+            sock = socket.socket(socket.PF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
+            sock.bind((interface,))
+            continue
+
+        can_time = time.time()
+        extended_frame,can_id, can_dlc, can_data, can_id_string = unpack_CAN(can_packet)
+        can_data_string = " ".join(["{:02X}".format(b) for b in can_data[:can_dlc]])
+        if extended_frame:
+            priority, pgn, da, sa = get_j1939_from_id(can_id)
+        else:
+            #11-bit IDs, so J1939 is not defined
+            priority = 0xE #out of range value
+            pgn = 0xFFFFE #made up no-sensical value
+            da = 0xFE #null
+            sa = 0xFE #null
+        try:
+            raw_data_queue.put((interface, sa, pgn, can_time, da, can_id_string, can_data_string, can_data[:can_dlc]))
+            summary_data['total_count']+=1
+            summary_data[interface]['count']+=1
+        except Exception as e:
+            logger.warning(f"Failed to put data in raw_data_queue: {str(e)}")
+
+def get_j1939_from_id(can_id):
+    priority = (PRIORITY_MASK & can_id) >> 26
+    PF = (can_id & PF_MASK) >> 16
+    if PF >= 0xF0:  # PDU 2 format
+        DA = 255
+        PGN = (can_id & PDU2_PGN_MASK) >> 8
+    else:  # PDU 1 format
+        PGN = (can_id & PDU1_PGN_MASK) >> 8
+        DA = (can_id & PS_MASK) >> 8
+    SA = (can_id & SA_MASK)
+    return priority, PGN, DA, SA
+
+def unpack_CAN(can_packet):
+    can_id, can_dlc, can_data = struct.unpack(can_frame_format, can_packet)
+    extended_frame = bool(can_id & socket.CAN_EFF_FLAG)
+    if extended_frame:
+        can_id &= socket.CAN_EFF_MASK
+        can_id_string = "{:08X}".format(can_id)
+    else:  # Standard Frame
+        can_id &= socket.CAN_SFF_MASK
+        can_id_string = "{:03X}".format(can_id)
+    return extended_frame,can_id, can_dlc, can_data, can_id_string
+
+def process_data():
+    
+    sa_data = {"Source":{}}
+    while True:
+        while not raw_data_queue.empty():
+            (interface, sa, pgn, can_time, da, can_id_string, can_data_string, can_data) = raw_data_queue.get()
+            processed_data_queue.put((interface, sa, pgn, can_time, da, can_id_string, can_data_string, can_data))
+        
+            try:
+                summary_data[interface]["source"][sa]['count'] += 1
+                sa_data["Source"][sa]['count'] += 1
+            except KeyError:
+                summary_data[interface]["source"][sa] = {'count': 1, 'address': sa, 'pgns': {}}
+                sa_data["Source"][sa] = {'count': 1, 'address': sa, 'pgns': {}}
+            try:
+                summary_data[interface]["source"][sa]['pgns'][pgn]['count'] += 1
+            except KeyError:
+                summary_data[interface]["source"][sa]['pgns'][pgn] = {'count': 1}
+                
+            summary_data[interface]["source"][sa]['pgns'][pgn]['time'] = can_time
+            summary_data[interface]["source"][sa]['pgns'][pgn]['id'] = can_id_string
+            summary_data[interface]["source"][sa]['pgns'][pgn]['da'] = da
+            summary_data[interface]["source"][sa]['pgns'][pgn]['data'] = can_data_string
+            summary_data[interface]["source"][sa]['pgns'][pgn]['time_delta'] = "{:d}ms".format(
+                    int((can_time - summary_data[interface]["source"][sa]['pgns'][pgn]['time']) * 1000))
+            
+            
+        socketio.emit('message', sa_data)  # Emit processed data to the WebSocket
+        time.sleep(STATS_UPDATE_PERIOD)
+
+def socket_can_data(interface):
+    while True:
+        data = get_can_stats(interface)
+        socketio.emit('can_stats', data)  # Emit processed data to the WebSocket
+        logger.debug(f"emitted can_stats: {data}")
+        time.sleep(1.09)
+
+def write_to_db(table_name):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute(f'''
+                CREATE TABLE IF NOT EXISTS {table_name} (
+                    interface TEXT,
+                    sa INTEGER,
+                    pgn INTEGER,
+                    timestamp FLOAT,
+                    da INTEGER,
+                    can_id TEXT,
+                    data_hex TEXT,
+                    data_bytes BLOB,
+                    PRIMARY KEY (interface, sa, pgn, timestamp)
+                )
+            ''')
+    conn.commit()
+    table_data = []
+    while True:
+        while not processed_data_queue.empty():
+            data = processed_data_queue.get() 
+            #data = (interface, sa, pgn, can_time, da, can_id, can_data_string, can_data)
+            table_data.append(data)
+        if logging_active.is_set():  # check to see if logging is active    
+            try:
+                cursor.execute('BEGIN;')
+                for item in table_data:
+                    cursor.execute(f'''
+                        INSERT INTO {table_name} (interface, sa, pgn, timestamp, da, can_id, data_hex, data_bytes)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', item)
+                cursor.execute('COMMIT;')
+            except sqlite3.Error as e:
+                cursor.execute('ROLLBACK;')
+                logger.warning(f"SQLite transaction error: {e}")
+        logger.debug(f"Inserted {len(table_data)} lines into the database.")
+        table_data = []
+        time.sleep(UPDATE_PERIOD)
+        
 
 
 ################################################
@@ -184,9 +362,9 @@ def get_ip():
     return jsonify({'ip': ip_address})
 
 
-def get_can_stats():
+def get_can_stats(interface):
     # Command to get CAN statistics with detailed information
-    command = "ip -details -statistics link show can0"
+    command = f"ip -details -statistics link show {interface}"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         return {"error": "Failed to retrieve CAN statistics"}
@@ -206,17 +384,17 @@ def get_can_stats():
             rx_header = stat_lines[line].strip().split()[1:] #Remove the "RX:"
             rx_line = stat_lines[line+1].strip().split()
             for k,v in zip(rx_header,rx_line):
-                stats['CANRX'+k] = humanize.naturalsize(int(v), binary=True)
+                stats['CANRX'+k] = f"{int(v)//1024}kB"
         elif 'TX:' in stat_lines[line]:
             tx_header = stat_lines[line].strip().split()[1:] #Remove the "TX:"
             tx_line = stat_lines[line+1].strip().split()
             for k,v in zip(tx_header,tx_line):
                 stats['CANTX'+k] = v
-    return stats
+    return {interface:stats}
 
 @app.route('/api/can_stats', methods=['GET'])
 def can_stats():
-    stats = get_can_stats()
+    stats = get_can_stats(interface)
     return jsonify(stats)
 
 def start_can(bitrate):
@@ -272,8 +450,80 @@ def system_shutdown():
     except Exception as e:
         return jsonify({"status": "error", "output": str(e)})
 
+@app.route('/start_logging', methods=['POST'])
+def start_logging():
+    logging_active.set()
+    return jsonify({"status": "Logging started"}), 200
+
+@app.route('/stop_logging', methods=['POST'])
+def stop_logging():
+    logging_active.clear()
+    return jsonify({"status": "Logging stopped"}), 200
+
+@app.route('/restart_logging', methods=['POST'])
+def restart_logging():
+    table_name = request.json.get('table_name')
+    if not table_name:
+        return jsonify({"error": "Table name is required"}), 400
+    
+    logging_active.clear()
+    global write_thread
+    write_thread = threading.Thread(target=write_to_db, args=(table_name,))
+    write_thread.daemon = True
+    write_thread.start()
+    logging_active.set()
+    return jsonify({"status": f"Logging restarted with table {table_name}"}), 200
+
+@app.route('/drop_table', methods=['POST'])
+def drop_table():
+    table_name = request.json.get('table_name')
+    if not table_name:
+        return jsonify({"error": "Table name is required"}), 400
+    
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute(f"DROP TABLE IF EXISTS {table_name}")
+    conn.commit()
+    conn.close()
+    return jsonify({"status": f"Table {table_name} dropped"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    #app.run(host='0.0.0.0', port=5000, debug=False)
+    # Define the threads for reading CAN data
+    logger.info(f"Setting up read and information threads for {can_interfaces}")
+    read_threads = []
+    info_threads = []
+    for interface in can_interfaces:
+        t = threading.Thread(target=read_can_data, args=(interface,))
+        info = threading.Thread(target=socket_can_data, args=(interface,))
+        t.daemon = True
+        info.daemon = True
+        read_threads.append(t)
+        info_threads.append(info)
+        t.start()
+        info.start()
+
+    logger.info(f"Setting up data processing thread.")
+    # Thread for processing data
+    process_thread = threading.Thread(target=process_data)
+    process_thread.daemon = True
+    process_thread.start()
+
+    # Thread for writing data to SQLite
+    logger.info(f"Starting database writing thread.")
+    write_thread = threading.Thread(target=write_to_db, args=("can_data",))
+    write_thread.daemon = True
+    write_thread.start()
+    
+    logger.info("Running flask app on port 5000")
+    # Run Flask app with SocketIO
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+    # Keep the main thread alive
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting...")
 
 
--- README.md
+++ README.md
@@ -1,2 +1,356 @@
 # BallastController
 A project to automatically control filling the ballast tanks on a wakesurf boat with extra bags installed.
+
+## Operating System Install
+1. The operating system on a Raspberry Pi runs from the SD card. Therefore, we need to start with an SD card image. For this project, we started with the 64-bit Raspberry Pi OS Lite.
+
+![pi imager](image.png)
+
+### Package Installs
+After booting the Pi, connecting to the Internet, and logging in, install the following packages:
+```
+sudo apt update
+sudo apt upgrade -y
+sudo apt install dnsmasq hostapd  dhcpcd5 -y
+sudo apt install iptables accountsservice -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit -y
+sudo apt install lightdm xorg openbox chromium-browser -y
+sudo apt install python3-pip python3-venv lighttpd -y
+sudo apt install python3-flask uwsgi uwsgi-plugin-python3 -y
+sudo apt install can-utils -y
+```
+
+### Enable CAN
+Using a Waveshare RE458 CAN Hat, enter the following in the boot config.txt file.
+```
+sudo nano /boot/firmware/config.txt
+```
+Uncomment and enter the lines:
+```
+dtparam=spi=on
+dtoverlay=mcp2515-can0,oscillator=12000000,interrupt=25
+dtoverlay=spi-bcm2835-overlay
+```
+Create a file named /etc/modules-load.d/can.conf to load the CAN modules at boot:
+```
+sudo nano /etc/modules-load.d/can.conf
+```
+Add the following lines:
+```
+spi-bcm2835
+mcp251x
+can-dev
+```
+Edit the /etc/network/interfaces.d/can0 file:
+```
+sudo nano /etc/network/interfaces.d/can0
+```
+Update the file to use the correct CAN interface configuration:
+```
+auto can0
+iface can0 can static
+    pre-up /sbin/ip link set can0 type can bitrate 250000 restart-ms 100
+    up /sbin/ifconfig can0 up
+    down /sbin/ifconfig can0 down
+```
+
+### Configurations
+Most of these settings were determined by prompting ChatGPT 4o.
+
+
+1. DHCP - gives an IP address to anyone connecting over wireless.
+```
+sudo nano /etc/dhcpcd.conf
+```
+Enter the following lines at the end of the file:
+```
+interface wlan0
+    static ip_address=192.168.4.1/24
+    nohook wpa_supplicant
+denyinterfaces can*
+```
+2. Host Access Point - makes a raspberry Pi a wireless access point so people can connect without going through a switch.
+```
+sudo nano /etc/hostapd/hostapd.conf
+```
+Enter these lines in the text editor.
+```
+interface=wlan0
+driver=nl80211
+ssid=new_ssid
+hw_mode=g
+channel=6
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=new_password
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+```
+
+```
+sudo nano /etc/default/hostapd
+```
+Add this line in the open file:
+```
+DAEMON_CONF="/etc/hostapd/hostapd.conf"
+```
+
+
+3. DNS
+```
+sudo nano /etc/dnsmasq.conf
+```
+Enter the following lines.
+```
+interface=wlan0      # Use the wireless interface
+  dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+```
+
+9. Start and Enable hostapd and dnsmasq
+
+Unmasking the hostapd service allows it to be started and enabled:
+```
+sudo systemctl unmask hostapd
+```
+
+Start and enable the services:
+```
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
+sudo systemctl enable hostapd
+sudo systemctl enable dnsmasq
+```
+
+4. Enable packet forwarding:
+```
+sudo nano /etc/sysctl.conf
+```
+```
+# Uncomment the next line to enable packet forwarding for IPv4
+net.ipv4.ip_forward=1
+```
+
+5. Restore IP tables on boot
+```
+sudo nano /etc/rc.local
+```
+Add the line before `exit 0`. 
+```
+iptables-restore < /etc/iptables.ipv4.nat
+```
+6. Prevent contention over the wlan0 interface
+```
+sudo nano /etc/NetworkManager/NetworkManager.conf
+```
+
+```
+[keyfile]
+unmanaged-devices=interface-name:wlan0
+```
+
+7. Get kisok to start
+```
+nano ~/.config/openbox/autostart
+```
+```
+#!/bin/bash
+xset -dpms      # Disable DPMS (Energy Star) features.
+xset s off      # Disable screen saver
+xset s noblank  # Don't blank the video device
+
+# Start Chromium in kiosk mode
+chromium-browser --noerrdialogs --kiosk http://localhost --incognito --disable-translate
+```
+
+8. Get the screen to rotate
+```
+sudo nano /usr/share/lightdm/display-setup-script.sh
+```
+```
+#!/bin/bash
+export DISPLAY=:0
+xrandr --output HDMI-1 --rotate right
+```
+
+```
+sudo nano /etc/lightdm/lightdm.conf
+```
+Under `[Seat:*]`, uncomment and edit these lines.
+```
+display-setup-script=/usr/share/lightdm/display-setup-script.sh
+autologin-user=pi
+autologin-session=openbox
+```
+
+9. Setup Flask
+```
+sudo nano /etc/systemd/system/myflaskapp.service
+```
+```
+[Unit]
+Description=uWSGI instance to serve flask app.py
+After=network.target
+
+[Service]
+User=pi
+Group=www-data
+WorkingDirectory=/home/pi
+ExecStart=/usr/bin/uwsgi --ini uwsgi.ini
+
+[Install]
+WantedBy=multi-user.target
+```
+
+```
+nano ~/uwsgi.ini
+```
+```
+[uwsgi]
+module = app:app
+master = true
+processes = 5
+socket = /tmp/myflaskapp.sock
+chmod-socket = 660
+vacuum = true
+die-on-term = true
+```
+
+
+```
+sudo systemctl daemon-reload
+sudo systemctl restart myflaskapp.service
+sudo systemctl status myflaskapp.service
+```
+
+### Give a wireless access point
+To debug or interface with the pi in the field, we need to connect to it.
+
+```
+sudo apt update
+sudo apt upgrade -y
+```
+
+```
+sudo apt install dnsmasq hostapd dhcpcd5 -y
+```
+
+```
+sudo nano /etc/dhcpcd.conf
+```
+add the following at the end of the file:
+
+```
+interface wlan0
+    static ip_address=192.168.4.1/24
+    nohook wpa_supplicant
+```
+
+Enable and start the dhcp service:
+```
+sudo systemctl enable dhcpcd
+sudo systemctl start dhcpcd
+```
+
+Configure  `dnsmasq`
+```
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo nano /etc/dnsmasq.conf
+```
+
+Put these lines in the file:
+```
+interface=wlan0      # Use the wireless interface
+  dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+```
+
+5. Configure hostapd
+```
+sudo nano /etc/hostapd/hostapd.conf
+```
+Add these lines:
+```
+Edit the hostapd configuration file:
+interface=wlan0
+driver=nl80211
+ssid=mastercraft
+hw_mode=g
+channel=5
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=password
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+```
+
+```
+sudo nano /etc/default/hostapd
+```
+Find the line starting with #DAEMON_CONF and replace it with:
+```
+DAEMON_CONF="/etc/hostapd/hostapd.conf"
+```
+THF90228
+
+7. Enable IP Forwarding
+
+Edit the sysctl.conf file to enable IP forwarding:
+```
+sudo nano /etc/sysctl.conf
+```
+Uncomment the following: 
+```
+net.ipv4.ip_forward=1
+```
+
+8. Configure NAT with iptables
+
+```
+sudo apt install iptables -y
+```
+Set up NAT (Network Address Translation) using iptables:
+```
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+```
+
+Save the iptables rule so it persists across reboots:
+```
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+```
+
+Edit the rc.local file to restore the iptables rule on boot:
+```
+sudo nano /etc/rc.local
+```
+Add the following line just above exit 0:
+```
+iptables-restore < /etc/iptables.ipv4.nat
+```
+
+
+
+
+Reboot
+```
+sudo shutdown -r now
+```
+
+Thqis should fix everything up so there is a wireless access point called `mastercraft` at `192.168.4.1` that provides access to the internet if the Ethernet is plugged into the Internet.
+
+## Additional Configuration options
+Run `raspi-config` and turn on SPI through the Interface Options.
+
+Enlarge root file system
+
+
+Adjust the font size:
+```
+sudo dpkg-reconfigure console-setup
+```
